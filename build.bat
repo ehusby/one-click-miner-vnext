336320bf3c7d7455eda3c01e60828a9c6bab429e
Erik Husby <husby.erik@gmail.com>
2021-03-17 21:52:17 -0500

    Fix binary name change in build.bat

--- build.bat
+++ build.bat
@@ -8,20 +8,20 @@ REN version.go version.go.build
 ECHO package tracking >> version.go
 ECHO var version="%ver%-%gitver%" >> version.go
 CD ..
-DEL build\vertcoin-ocm.exe
+DEL build\verthash-ocm.exe
 wails build
 ECHO "Sign the release assembly now on the windows machine if desired, then:"
 PAUSE
-CD build 
-COPY vertcoin-ocm.exe ..\vertcoin-ocm-%ver%-windows-x64.exe
-7z a ../vertcoin-ocm-%ver%-windows-x64.zip vertcoin-ocm.exe 
+CD build
+COPY verthash-ocm.exe ..\verthash-ocm-%ver%-windows-x64.exe
+7z a ../verthash-ocm-%ver%-windows-x64.zip verthash-ocm.exe
 CD ..
 wails build -d
 ECHO "Sign the debug assembly now on the windows machine if desired, then:"
 PAUSE
-CD build 
-COPY vertcoin-ocm.exe ..\vertcoin-ocm-%ver%-windows-x64-debug.exe
-7z a ../vertcoin-ocm-%ver%-windows-x64-debug.zip vertcoin-ocm.exe 
+CD build
+COPY verthash-ocm.exe ..\verthash-ocm-%ver%-windows-x64-debug.exe
+7z a ../verthash-ocm-%ver%-windows-x64-debug.zip verthash-ocm.exe
 CD ../tracking
 DEL version.go
 REN version.go.build version.go
